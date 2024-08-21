9d18deb4ff64b4754ebb3d8d1d9a138c0305ca65
Brook Worku <brookwworku3@gmail.com>
2024-08-21 08:50:46 +0300

    next js initalization

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Brook's Protfolio",
+  description: "A personal portfolio of Brook Worku Alemu",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex p-4 text-xl font-semibold items-center justify-center">
+      Hello Mrs
+      
+    </main>
+  );
+}
